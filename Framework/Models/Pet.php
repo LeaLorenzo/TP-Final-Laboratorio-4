d113cc81c6506ba8ac2a18598a5e79c0301cd3fe
Leandro Lorenzo <leandromartinlorenzo@gmail.com>
2022-10-15 20:03:20 -0300

    Creación de Clases en Models
    Se crearon las clases en la carpeta Models. Hay que ajustar los requerimientos de fotos y definir incumbencias de atributos entre pets y keepers.

---
+++ Framework/Models/Pet.php
@@ -0,0 +1,69 @@
+<?php
+namespace Models;
+class Pet
+{
+    private $photo; // TODO averiguar qué hacer con las fotos de las mascotas
+    private $breed;
+    private $size; // existe size en la clase Keeper, creo que le corresponde a Pet
+    private $healthBook; // TP pide imagen
+    private $observation;
+
+    function __construct($breed,$size,$healthBook,$observation)
+    {
+        $this->breed = $breed;
+        $this->size = $size;
+        $this->healthBook = $healthBook;
+        $this->observation = $observation;
+    }
+
+    public function getPhoto()
+    {
+        return $this->photo;
+    } 
+
+    public function setPhoto($photo)
+    {
+        $this->photo = $photo;
+    }
+
+    public function getBreed()
+    {
+        return $this->breed;
+    }
+
+    public function setBreed($breed)
+    {
+        $this->breed = $breed;
+    }
+
+    public function getSize()
+    {
+        return $this->size;
+    }
+
+    public function setSize($size)
+    {
+        $this->size = $size;
+    }
+
+    public function getHealthBook()
+    {
+        return $this->healthBook;
+    }
+
+    public function setHealthBook($healthBook)
+    {
+        $this->healthBook = $healthBook;
+    }
+
+    public function getObservation()
+    {
+        return $this->observation;
+    }
+
+    public function setObservation($observation)
+    {
+        $this->observation = $observation;
+    }
+}
+?>
